f3d8908c583ec3627442d1671f1e50218a6455da
unknown <lucasocanto1996@gmail.com>
2021-10-10 23:25:09 -0300

    subiendo ml

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+
+const publicPath = path.resolve(__dirname,"./public")
+app.use(express.static(publicPath))
+app.use(express.urlencoded())
+
+app.get("/", (req,res)=>{
+    let file = path.join(__dirname,"./views/home.html")
+    res.sendFile(file)
+})
+
+app.get("/register",(req,res)=>{
+    res.sendFile(__dirname + "/views/register.html")
+})
+
+app.post("/resultado-register",(req,res)=>{
+    res.redirect("/")
+})
+
+app.get("/login",(req,res)=>{
+    res.sendFile(__dirname + "/views/login.html")
+})
+
+app.post("/resultado-login",(req,res)=>{
+    res.redirect("/")
+})
+
+app.listen(3030,()=>{
+    console.log("Server running")
+})
